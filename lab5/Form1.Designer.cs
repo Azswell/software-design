77a1c266afbe5bd7eaf9f3b2cdc9728f404162a7
Azswell <kurt.galanida@gmail.com>
2026-02-27 20:50:27 +0800

    -week 5 finish

--- lab5/Form1.Designer.cs
+++ lab5/Form1.Designer.cs
@@ -1,39 +1,268 @@
 ﻿namespace lab5
 {
-	partial class Form1
-	{
-		/// <summary>
-		///  Required designer variable.
-		/// </summary>
-		private System.ComponentModel.IContainer components = null;
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
 
-		/// <summary>
-		///  Clean up any resources being used.
-		/// </summary>
-		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		protected override void Dispose(bool disposing)
-		{
-			if (disposing && (components != null))
-			{
-				components.Dispose();
-			}
-			base.Dispose(disposing);
-		}
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
-		}
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            grpbxcSearch = new GroupBox();
+            btnSearch = new Button();
+            txtbxSearch = new TextBox();
+            grpbxAdd = new GroupBox();
+            lblBookTitle = new Label();
+            lblAuthorName = new Label();
+            lblBookId = new Label();
+            btnAdd = new Button();
+            txtbxBookTitle = new TextBox();
+            txtbxAuthorName = new TextBox();
+            txtbxBookId = new TextBox();
+            grpbxDelete = new GroupBox();
+            btnDelete = new Button();
+            txtbxDelBookId = new TextBox();
+            grpbxLibrary = new GroupBox();
+            listbxTable = new ListBox();
+            btnUpdate = new Button();
+            btnShow = new Button();
+            grpbxcSearch.SuspendLayout();
+            grpbxAdd.SuspendLayout();
+            grpbxDelete.SuspendLayout();
+            grpbxLibrary.SuspendLayout();
+            SuspendLayout();
+            // 
+            // grpbxcSearch
+            // 
+            grpbxcSearch.Controls.Add(btnSearch);
+            grpbxcSearch.Controls.Add(txtbxSearch);
+            grpbxcSearch.Location = new Point(12, 12);
+            grpbxcSearch.Name = "grpbxcSearch";
+            grpbxcSearch.Size = new Size(383, 82);
+            grpbxcSearch.TabIndex = 0;
+            grpbxcSearch.TabStop = false;
+            grpbxcSearch.Text = "Search by Author";
+            // 
+            // btnSearch
+            // 
+            btnSearch.Location = new Point(282, 32);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(95, 23);
+            btnSearch.TabIndex = 1;
+            btnSearch.Text = "Search";
+            btnSearch.UseVisualStyleBackColor = true;
+            btnSearch.Click += btnSearch_Click;
+            // 
+            // txtbxSearch
+            // 
+            txtbxSearch.Location = new Point(15, 32);
+            txtbxSearch.Name = "txtbxSearch";
+            txtbxSearch.Size = new Size(261, 23);
+            txtbxSearch.TabIndex = 0;
+            // 
+            // grpbxAdd
+            // 
+            grpbxAdd.Controls.Add(lblBookTitle);
+            grpbxAdd.Controls.Add(lblAuthorName);
+            grpbxAdd.Controls.Add(lblBookId);
+            grpbxAdd.Controls.Add(btnAdd);
+            grpbxAdd.Controls.Add(txtbxBookTitle);
+            grpbxAdd.Controls.Add(txtbxAuthorName);
+            grpbxAdd.Controls.Add(txtbxBookId);
+            grpbxAdd.Location = new Point(12, 100);
+            grpbxAdd.Name = "grpbxAdd";
+            grpbxAdd.Size = new Size(383, 229);
+            grpbxAdd.TabIndex = 1;
+            grpbxAdd.TabStop = false;
+            grpbxAdd.Text = "Add a Book";
+            // 
+            // lblBookTitle
+            // 
+            lblBookTitle.AutoSize = true;
+            lblBookTitle.Location = new Point(15, 134);
+            lblBookTitle.Name = "lblBookTitle";
+            lblBookTitle.Size = new Size(59, 15);
+            lblBookTitle.TabIndex = 8;
+            lblBookTitle.Text = "Book Title";
+            // 
+            // lblAuthorName
+            // 
+            lblAuthorName.AutoSize = true;
+            lblAuthorName.Location = new Point(15, 84);
+            lblAuthorName.Name = "lblAuthorName";
+            lblAuthorName.Size = new Size(79, 15);
+            lblAuthorName.TabIndex = 7;
+            lblAuthorName.Text = "Author Name";
+            // 
+            // lblBookId
+            // 
+            lblBookId.AutoSize = true;
+            lblBookId.Location = new Point(15, 32);
+            lblBookId.Name = "lblBookId";
+            lblBookId.Size = new Size(50, 15);
+            lblBookId.TabIndex = 6;
+            lblBookId.Text = "Book Id:";
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(290, 191);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(75, 23);
+            btnAdd.TabIndex = 5;
+            btnAdd.Text = "Add";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // txtbxBookTitle
+            // 
+            txtbxBookTitle.Location = new Point(15, 152);
+            txtbxBookTitle.Name = "txtbxBookTitle";
+            txtbxBookTitle.Size = new Size(350, 23);
+            txtbxBookTitle.TabIndex = 4;
+            // 
+            // txtbxAuthorName
+            // 
+            txtbxAuthorName.Location = new Point(15, 102);
+            txtbxAuthorName.Name = "txtbxAuthorName";
+            txtbxAuthorName.Size = new Size(350, 23);
+            txtbxAuthorName.TabIndex = 3;
+            // 
+            // txtbxBookId
+            // 
+            txtbxBookId.Location = new Point(15, 50);
+            txtbxBookId.Name = "txtbxBookId";
+            txtbxBookId.Size = new Size(350, 23);
+            txtbxBookId.TabIndex = 2;
+            // 
+            // grpbxDelete
+            // 
+            grpbxDelete.Controls.Add(btnDelete);
+            grpbxDelete.Controls.Add(txtbxDelBookId);
+            grpbxDelete.Location = new Point(12, 335);
+            grpbxDelete.Name = "grpbxDelete";
+            grpbxDelete.Size = new Size(383, 103);
+            grpbxDelete.TabIndex = 2;
+            grpbxDelete.TabStop = false;
+            grpbxDelete.Text = "Delete a Book";
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(290, 71);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(75, 23);
+            btnDelete.TabIndex = 6;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
+            // 
+            // txtbxDelBookId
+            // 
+            txtbxDelBookId.Location = new Point(15, 40);
+            txtbxDelBookId.Name = "txtbxDelBookId";
+            txtbxDelBookId.Size = new Size(350, 23);
+            txtbxDelBookId.TabIndex = 6;
+            // 
+            // grpbxLibrary
+            // 
+            grpbxLibrary.Controls.Add(listbxTable);
+            grpbxLibrary.Controls.Add(btnUpdate);
+            grpbxLibrary.Controls.Add(btnShow);
+            grpbxLibrary.Location = new Point(401, 12);
+            grpbxLibrary.Name = "grpbxLibrary";
+            grpbxLibrary.Size = new Size(387, 426);
+            grpbxLibrary.TabIndex = 3;
+            grpbxLibrary.TabStop = false;
+            grpbxLibrary.Text = "Library";
+            // 
+            // listbxTable
+            // 
+            listbxTable.FormattingEnabled = true;
+            listbxTable.ItemHeight = 15;
+            listbxTable.Location = new Point(6, 22);
+            listbxTable.Name = "listbxTable";
+            listbxTable.Size = new Size(375, 364);
+            listbxTable.TabIndex = 9;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Location = new Point(204, 394);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(75, 23);
+            btnUpdate.TabIndex = 8;
+            btnUpdate.Text = "Update";
+            btnUpdate.UseVisualStyleBackColor = true;
+            btnUpdate.Click += btnUpdate_Click;
+            // 
+            // btnShow
+            // 
+            btnShow.Location = new Point(285, 394);
+            btnShow.Name = "btnShow";
+            btnShow.Size = new Size(96, 23);
+            btnShow.TabIndex = 7;
+            btnShow.Text = "Show Books";
+            btnShow.UseVisualStyleBackColor = true;
+            btnShow.Click += btnShow_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(grpbxLibrary);
+            Controls.Add(grpbxDelete);
+            Controls.Add(grpbxAdd);
+            Controls.Add(grpbxcSearch);
+            Name = "Form1";
+            Text = "Form1";
+            grpbxcSearch.ResumeLayout(false);
+            grpbxcSearch.PerformLayout();
+            grpbxAdd.ResumeLayout(false);
+            grpbxAdd.PerformLayout();
+            grpbxDelete.ResumeLayout(false);
+            grpbxDelete.PerformLayout();
+            grpbxLibrary.ResumeLayout(false);
+            ResumeLayout(false);
+        }
 
-		#endregion
-	}
+        #endregion
+
+        private GroupBox grpbxcSearch;
+        private Button btnSearch;
+        private TextBox txtbxSearch;
+        private GroupBox grpbxAdd;
+        private Button btnAdd;
+        private TextBox txtbxBookTitle;
+        private TextBox txtbxAuthorName;
+        private TextBox txtbxBookId;
+        private GroupBox grpbxDelete;
+        private Button btnDelete;
+        private TextBox txtbxDelBookId;
+        private GroupBox grpbxLibrary;
+        private ListBox listbxTable;
+        private Button btnUpdate;
+        private Button btnShow;
+        private Label lblBookTitle;
+        private Label lblAuthorName;
+        private Label lblBookId;
+    }
 }
--- lab5/Form1.cs
+++ lab5/Form1.cs
@@ -1,10 +1,158 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
 namespace lab5
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public class Author
+        {
+            public int AuthorId { get; set; }
+            public string Name { get; set; }
+
+            public virtual ICollection<Book> Books { get; set; }
+        }
+
+        public class Book
+        {
+            public int BookId { get; set; }
+            public string Title { get; set; }
+
+            public int AuthorId { get; set; }
+
+            public virtual Author Author { get; set; }
+        }
+
+        private void AddAuthorWithBook(string authorName, string bookTitle)
+        {
+            using (var _context = new BookstoreContext())
+            {
+                var author = new Author { Name = authorName };
+                var book = new Book { Title = bookTitle, Author = author };
+
+                _context.Authors.Add(author);
+                _context.Books.Add(book);
+                _context.SaveChanges();
+            }
+        }
+
+        private List<string> GetBooksWithAuthors()
+        {
+            using (var _context = new BookstoreContext())
+            {
+                var bookWithAuthors = _context.Books
+                    .Include(book => book.Author)
+                    .Select(book => $"Id[{book.BookId}] {book.Title} by {book.Author.Name}")
+                    .ToList();
+
+                return bookWithAuthors;
+            }
+
+        }
+
+        private void UpdateBookAndAuthor(int bookId, string newTitle, string authorName)
+        {
+            using (var _context = new BookstoreContext())
+            {
+                var book = _context.Books.Include(book => book.Author).FirstOrDefault(book => book.BookId == bookId);
+                if (book != null)
+                {
+                    book.Title = newTitle;
+                    book.Author.Name = authorName;
+                    _context.SaveChanges();
+                }
+            }
+        }
+
+        private void DeleteBookById(int bookId)
+        {
+            using (var _context = new BookstoreContext())
+            {
+
+                var book = _context.Books.Find(bookId);
+
+                if (book != null)
+                {
+                    _context.Books.Remove(book);
+                    _context.SaveChanges();
+                    MessageBox.Show($"{book.Title} with ID {bookId} has been deleted.");
+                }
+                else
+                {
+                    MessageBox.Show("Book ID not found.");
+                }
+            }
+        }
+
+        private List<string> SearchBooksbyAuthor(string authorName)
+        {
+            using (var _context = new BookstoreContext())
+            {
+                var books = _context.Books
+                    .Include(book => book.Author)
+                    .Where(book => book.Author.Name.Contains(authorName))
+                    .Select(book => $"Id[{book.BookId}] - {book.Title} by {book.Author.Name}")
+                    .ToList();
+
+                return books;
+            }
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            var books = GetBooksWithAuthors();
+            listbxTable.DataSource = books;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            AddAuthorWithBook(txtbxAuthorName.Text, txtbxBookTitle.Text);
+            MessageBox.Show($"{txtbxBookTitle.Text} by {txtbxAuthorName.Text} has been added to the Library");
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int bookId = int.Parse(txtbxBookId.Text);
+            UpdateBookAndAuthor(bookId, txtbxBookTitle.Text, txtbxAuthorName.Text);
+            MessageBox.Show("Book and Author has been updated");
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int bookid = int.Parse(txtbxDelBookId.Text);
+            DeleteBookById(bookid);
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string searchName = txtbxSearch.Text;
+            if (!string.IsNullOrWhiteSpace(searchName))
+            {
+                var filteredbooks = SearchBooksbyAuthor(searchName);
+                listbxTable.DataSource = filteredbooks;
+
+                if(filteredbooks.Count == 0)
+                {
+                    MessageBox.Show("No books has been found for this author");
+                }
+                
+            }
+
+        }
+    }
 }
